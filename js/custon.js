67e516144b25f0f81a8694bba52994cca9262e81
arvinsblog <958655269@qq.com>
2020-12-07 11:05:05 +0800

    Site updated: 2020-12-07 11:05:00

--- js/custon.js
+++ js/custon.js
@@ -32,3 +32,9 @@ window.onload = function() {
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(bp, s);
 })();
+
+
+// 设置页脚博主会动的心
+$(document).ready(function(e) {
+  $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Arvin');
+});
